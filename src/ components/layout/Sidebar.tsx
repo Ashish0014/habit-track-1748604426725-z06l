008250b741acad221a0210298b8133f13b906f22
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:57:42 +0530

    Add src/ components/layout/Sidebar.tsx

---
+++ src/ components/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <aside className="w-64 bg-gray-900 text-white">
+      {/* Sidebar content */}
+    </aside>
+  );
+};
+
+export default Sidebar;
