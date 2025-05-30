8d66ba2b64c7b207be300cd5918f1d5d47608da9
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:57:45 +0530

    Add src/ App.tsx

---
+++ src/ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './components/layout/Layout';
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      {/* Main content goes here */}
+    </Layout>
+  );
+};
+
+export default App;
